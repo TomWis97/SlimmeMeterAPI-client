cd6ecd5cf90df81594d8ab420a602c572533197c
TomWis97 <github@tomwishaupt.nl>
2016-07-27 17:20:15 +0200

    Should be working or something idk

--- SlimmeMeterAPI-client/daemon.py
+++ SlimmeMeterAPI-client/daemon.py
@@ -1,11 +1,72 @@
 import configparser
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import urllib.parse
+import interpreter
+from mysql.connector import connection
+from datetime import datetime
 
 # Configuration reader
+config = configparser.ConfigParser()
+config.read('config.ini')
 httpPort = int(config['client']['port'])
+historyItems = config['client']['historyItems'].split(',')
 # Configuration reading done.
+
+# Initiate MySQL connection.
+cnx = connection.MySQLConnection(
+    user = config['client']['dbUser'],
+    password = config['client']['dbPassword'],
+    host = config['client']['dbHost'],
+    database = config['client']['dbdatabase']
+)
+cursor = cnx.cursor()
+
+def processData(input):
+    timestamp = ''
+    historyToSave = []
+    for item in input:
+        if item['name'] == 'timestamp':
+            timestamp = interpreter.converttimestamp(item['value'][0])
+        if item['name'] in historyItems:
+            # Interpreting each item  
+            historyToSave.append(interpreter.interpretValue(item['name'], item['value']))
+    for historyItem in historyToSave:
+        # TODO: Instead of lastreading, look at configuration file.
+        if historyItem['name'][1:] == 'lastReading':
+            # Checking if last information in database is the same as current information.
+            query = 'SELECT MAX(timestamp) FROM history WHERE name = %s'
+            cursor.execute(query, (historyItem['name'],))
+            result = cursor.fetchone()
+            if (result[0] == None):
+                query = "INSERT INTO history ( timestamp, name, value ) VALUES ( %s, %s, %s )"
+                cursor.execute(query, (historyItem['timestamp'], historyItem['name'],  historyItem['value']))
+            else:
+                # This doesn't preserve seconds. But that shouldn't matter because these values
+                # should only refresh once per hour.
+                lastInDb = result[0].strftime('%Y-%m-%d %H:%M:%S')
+                if lastInDb > interpreter.converttimestamp(historyItem['timestamp']):
+                    print("Writing to DB!", datetime.datetime())
+                    query = "INSERT INTO history ( timestamp, name, value ) VALUES ( %s, %s, %s )"
+                    cursor.execute(query, (historyItem['timestamp'], historyItem['name'],  historyItem['value']))
+        else:
+            query = "INSERT INTO history ( timestamp, name, value ) VALUES ( %s, %s, %s )"
+            cursor.execute(query, (timestamp, historyItem['name'],  historyItem['value']))
+    cnx.commit()
 
 class webserverHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        print("POST detected!1")
+        # TODO: Fix this MASSIVE security hole. Do as I say, not as I do.
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes('jeej', 'ASCII'))
+        length = self.headers['content-length']
+        data = self.rfile.read(int(length))
+        datastring = data.decode('UTF-8')
+        #print("datastring", datastring)
+        inputJson = json.loads(data.decode('UTF-8'))
+        processData(inputJson)
+
+myServer = HTTPServer(('', httpPort), webserverHandler)
+myServer.serve_forever()
---
+++ SlimmeMeterAPI-client/interpreter.py
@@ -0,0 +1,17 @@
+def interpretValue(name, value):
+    """Interprets values. name should be string. Value should be list or tuple.
+       Returns dictionary with fixed values."""
+    # Removing units.
+    if name[1:] == 'lastReading':
+        fixedValue = value[1].split('*')[0]
+        # lastReading needs timestamp processing.
+        fixedTimestamp = converttimestamp(value[0])
+        return {'name': name, 'value': fixedValue, 'timestamp': fixedTimestamp}
+    else:
+        fixedValue = value[0].split('*')[0]
+        return {'name': name, 'value': fixedValue}
+
+def converttimestamp(value):
+    """Converts a timestamp from YYMMDDHHmmSSX to MySQL timestamp."""
+    # TODO Interpret DST.
+    return '20' + value[0:2] + '-' + value[2:4] + '-' + value[4:6] + ' ' + value[6:8] + ':' + value[8:10] + ':' + value[10:12]
